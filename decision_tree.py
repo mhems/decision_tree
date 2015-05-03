a435b98e849d04835f8eed068704135f0a7f78b6
Matthew <mhems1@binghamton.edu>
2015-05-03 15:48:39 -0400

    post prune v1

--- decision_tree.py
+++ decision_tree.py
@@ -12,11 +12,16 @@ SALAMI_path = '/home/matt/Development/cs580/project/repo/salami_data/'
 val_path = SALAMI_path + 'validations/'
 run_path = SALAMI_path + 'runs/'
 
-BY_GAIN    = False
-BY_FREQ    = False
-SAVE_GRAPH = False
-MIN_GAIN   = 0.125
-MIN_FREQ   = 0.9
+BY_GAIN     = False
+BY_FREQ     = False
+GEN_VAL_SET = False
+POST_PRUNE  = False
+SAVE_GRAPH  = False
+MIN_GAIN    = 0.125
+MIN_FREQ    = 0.9
+
+def getBaseName(filename):
+    return re.sub('.*/', '', filename)
 
 def entropy(array):
     freq_dict = {}
@@ -61,19 +66,57 @@ def find_optimal_split(dataset):
     
 class DTreeNode:
     i = 0
-    def __init__(self,val,col=-1,l=None,r=None):
-        self.value = val
-        self.col = col
-        self.left = l
-        self.right = r
-        self.count = DTreeNode.i
+
+    def __init__(self, val, col, maj, l, r):
+        self.value                = val
+        self.col                  = col
+        self.majority_class       = maj
+        self.left                 = l
+        self.right                = r
+        self.count                = DTreeNode.i
+        self.classification_error = 0
+        self.num_seen             = 0
+        self.prune_error          = 0
+
+    @classmethod
+    def makeLeaf(cls, val):
+        return cls(val, -1, val, None, None)
 
     def decide (self, datarow):
+        truth = datarow['genre']
+        self.num_seen += 1
         if self.isLeaf():
-            return self.value
-        if (datarow[self.col] < self.value):
-            return self.left
-        return self.right
+            ret = self.value
+        elif (datarow[self.col] < self.value):
+            ret = self.left.decide(datarow)
+        else:
+            ret = self.right.decide(datarow)
+        if ret != truth:
+            self.classification_error += 1
+        if self.majority_class != truth:
+            self.prune_error += 1
+        return ret
+
+    def getMaxReducingNode(self):
+        if self.isLeaf():
+            return self
+        else:
+            left_n    = self.left.getMaxReducingNode()
+            right_n   = self.right.getMaxReducingNode()
+            left_red  = left_n.classification_error  - left_n.prune_error
+            right_red = right_n.classification_error - right_n.prune_error
+            my_red    = self.classification_error    - self.prune_error
+            max_red   = max(left_red, right_red, my_red)
+            if   max_red == left_red:
+                return left_n
+            elif max_red == right_red:
+                return right_n
+            else:
+                return self
+
+    def prune(self):
+        print 'Pruning to %s, reduction: %d' % (self.majority_class, self.classification_error - self.prune_error)
+        self = DTreeNode.makeLeaf(self.majority_class)
 
     def isLeaf(self):
         return self.left is None and self.right is None
@@ -137,10 +180,11 @@ class DTree:
         print 'Learning decision tree'
         graph = pydot.Dot(graph_type='digraph')
         self.root = learn_decision_tree(dataframe, graph)
-        print 'Decision tree for %s has been learned' % filename
+        print 'Decision tree for %s has been learned' % getBaseName(filename)
         print 'Max. depth: %d' % self.length()
         if val_fn is not None:
-            # post prune
+            self.post_prune()
+            print 'Pruned depth: %d' % self.length()
         if SAVE_GRAPH:
             graph.write_png('%s.png' % filename)
         
@@ -153,13 +197,10 @@ class DTree:
         return max(self.__len(node.left), self.__len(node.right)) + 1
 
     def decide (self, datarow):
-        return self.decide_rec(self.root, datarow)
+        return self.root.decide(datarow)
 
-    def decide_rec(self, node, datarow):
-        dec = node.decide(datarow)
-        if type(dec) == type(""):
-            return dec
-        return self.decide_rec(dec, datarow)
+    def post_prune(self):
+        node = self.root.getMaxReducingNode()
 
     def prettyPrint(self):
         self.rec_print(self.root,"")
@@ -174,48 +215,36 @@ class DTree:
         else:
             print s + 'return ' + repr(node.value)
 
+def getMajorityClass(dataset):
+    grps = dataset.groupby(TARGET).groups
+    max_val = 0
+    for cat in grps:
+        if len(grps[cat]) > max_val:
+            max_val = len(grps[cat])
+            max_col = cat
+    return max_col, max_val
+
 def learn_decision_tree(dataset, graph):
     if num_groups(dataset) == 1:
         cls = re.split('[ \t\n\r]+', repr(dataset['genre']))[1]
         #print("  LEAF: '%s'" % (cls))
         DTreeNode.i += 1
-        return DTreeNode(cls)
+        return DTreeNode.makeLeaf(cls)
     #print("  INTERNAL %d %d" % (num_groups(dataset),len(dataset)))
     gain,axis,threshold = find_optimal_split(dataset)
     if axis == None or axis == '':
         raise Exception
+    max_col, max_val = getMajorityClass(dataset)
+    N = len(dataset)
     if BY_GAIN and BY_FREQ:
+        if gain < MIN_GAIN and max_val * 1.0 / N > MIN_FREQ:
+            return DTreeNode.makeLeaf(max_col)
+    elif BY_GAIN:
         if gain < MIN_GAIN:
-            grps = dataset.groupby(TARGET).groups
-            N = len(dataset)
-            max_val = 0
-            for cat in grps:
-                if len(grps[cat]) > max_val:
-                    max_val = len(grps[cat])
-                    max_col = cat
-            print max_val * 1.0 / N
-            if max_val * 1.0 / N > 0.9:
-                return DTreeNode(max_col)
-    if BY_GAIN:
-        if gain < MIN_GAIN:
-            grps = dataset.groupby(TARGET).groups
-            max_val = 0
-            for cat in grps:
-                if len(grps[cat]) > max_val:
-                    max_val = len(grps[cat])
-                    max_col = cat
-            return DTreeNode(max_col)
-    if BY_FREQ:
-        grps = dataset.groupby(TARGET).groups
-        N = len(dataset)
-        max_val = 0
-        for cat in grps:
-            if len(grps[cat]) > max_val:
-                max_val = len(grps[cat])
-                max_col = cat
-        print max_val * 1.0 / N
+            return DTreeNode.makeLeaf(max_col)
+    elif BY_FREQ:
         if max_val * 1.0 / N > MIN_FREQ:
-            return DTreeNode(max_col)
+            return DTreeNode.makeLeaf(max_col)
     left_subset  = dataset[dataset[axis] <  threshold]
     left_node    = learn_decision_tree(left_subset, graph)
     right_subset = dataset[dataset[axis] >= threshold]    
@@ -225,26 +254,30 @@ def learn_decision_tree(dataset, graph):
     right_vertex = right_node.getVertex()
     graph.add_node(left_vertex)
     graph.add_node(right_vertex)
-    parent = DTreeNode(threshold, axis, left_node, right_node)
+    parent = DTreeNode(threshold, axis, max_col, left_node, right_node)
     parent_vertex = parent.getVertex()
     graph.add_node(parent_vertex)
     graph.add_edge(pydot.Edge(parent_vertex, left_vertex))
     graph.add_edge(pydot.Edge(parent_vertex, right_vertex))
     return parent
 
-def test_tree (train_fn,test_fn):
-    dTree = DTree(train_fn)
+def test_tree (train_fn, test_fn, val_fn):
+    dTree = DTree(train_fn, val_fn)
     df    = pd.read_csv(test_fn)
+    return evaluate(test_fn, dTree, df)
+
+# score dTree against df truth
+def evaluate(filename, dTree, df):
     wrong = 0
     total = len(df)
-    for _,row in df.iterrows():
+    for _, row in df.iterrows():
         ID = row['ID']
         guess = dTree.decide(row)
         truth = row['genre']
 #        print ID, guess, truth
         if guess != truth:
             wrong += 1
-    print 'File %s:: %d incorrect out of %d (%.2f%% correct)' % (test_fn,wrong,total, (total-wrong) * 100.0 / total)
+    print 'File %s:: %d incorrect out of %d (%.2f%% correct)' % (getBaseName(filename), wrong, total, (total-wrong) * 100.0 / total)
     return (wrong,total)
 
 def getContiguousPartitions(lines, chunksize):
@@ -276,32 +309,17 @@ def getRandomPartitions(lines, chunksize, K):
     ret.append(lines)
     return ret
 
-# given filename and K (num. chunks), make files
-def gen_cross_validation_files(filename, K):
-    superfile = open(filename,'r')
-    all_lines = superfile.readlines()
-    header = all_lines[0]
-    lines  = all_lines[1:]
-    num_lines = len(lines)
-    chunksize = num_lines/K
-#    chunks = getContiguousPartitions(lines, chunksize)
-    chunks = getRandomPartitions(lines, chunksize, K)
-    for i, c in enumerate(chunks):
-        test_f = open("%stest_%d.csv" % (val_path,i), 'w')
-        test_f.write(header)
-        test_f.writelines(c)
-        rest = []
-        [rest.extend(r) for r in chunks[0:i]]
-        [rest.extend(r) for r in chunks[i+1:]]
-        train_f = open("%strain_%d.csv" % (val_path,i), 'w')
-        train_f.write(header)
-        train_f.writelines(rest)
-
-def cross_validate(K):
+def cross_validate(filepath, K):
     acc_wrong = 0.0
     acc_total = 0.0
-    for i in range(0,K):
-        wrong, total = test_tree('%strain_%d.csv' % (val_path,i), '%stest_%d.csv' % (val_path,i))
+    for i in range(K):
+        suffix = '_%d.csv' % i
+        train_fn = filepath + 'train' + suffix
+        test_fn  = filepath + 'test'  + suffix
+        val_fn   = None
+        if POST_PRUNE:
+            val_fn   = filepath + 'val' + suffix
+        wrong, total = test_tree(train_fn, test_fn, val_fn)
         acc_wrong += wrong
         acc_total += total
     err = acc_wrong/acc_total
@@ -351,26 +369,20 @@ def gen_cross_validation_files(filename, K):
         test_f = open("%stest_%d.csv" % (val_path,i), 'w')
         test_f.write(header)
         test_f.writelines(c)
+        r = -1
+        if GEN_VAL_SET:
+            r = int(rand()*K)
+            while r == i:
+                r = int(rand()*K)
+            val_f = open('%sval_%d.csv' % (val_path, i), 'w')
+            val_f.write(header)
+            val_f.writelines(chunks[r])
         rest = []
-        [rest.extend(r) for r in chunks[0:i]]
-        [rest.extend(r) for r in chunks[i+1:]]
+        [rest.extend(chunks[idx]) for idx in range(K) if idx != i and idx != r]
         train_f = open("%strain_%d.csv" % (val_path,i), 'w')
         train_f.write(header)
         train_f.writelines(rest)
 
-def cross_validate(K):
-    acc_wrong = 0.0
-    acc_total = 0.0
-    for i in range(0,K):
-        wrong, total = test_tree('%strain_%d.csv' % (val_path,i), '%stest_%d.csv' % (val_path,i))
-        acc_wrong += wrong
-        acc_total += total
-    err = acc_wrong/acc_total
-    print "Total error: %.2f%%" % (err*100)
-    return err
-
-# filepath points to train and test files for cv
-# splits train into 1/5 4/5 tests for post-pruning
 def test_pruned_tree(filepath):
     for i in range(0,10):
         suffix = '_%d.csv' % i
@@ -379,37 +391,36 @@ def test_pruned_tree(filepath):
         test_fn  = filepath + 'test'  + suffix
         tree = DTree(train_fn, val_fn)
         df   = pd.read_csv(test_fn)
-        total = len(df)
-        wrong = 0
-        for _, row in df.iterrows():
-            ID = row['ID']
-            guess = dTree.decide(row)
-            truth = row['genre']
-            if guess != truth:
-                wrong += 1
-        print 'File %s:: %d incorrect out of %d (%.2f%% correct)' % (val_fn,wrong, total, (total-wrong) * 100.0 / total)
+        w, t = evaluate(val_fn, tree, df)
+        wrong += w
+        total += t
+    print 'Averaged %d incorrect out of total %d (%f%%)' % (wrong, total, (wrong * 100.0)/total)
     return (wrong, total)
 
 if __name__ == '__main__':
+    K = 10
     argc = len(sys.argv)
     if argc < 2 or argc > 3:
         print 'Usage: %s -m|-v|-r [-g|-f]' % sys.argv[0]
         sys.exit(1)
     if sys.argv[1] == '-m':
-        gen_cross_validation_files(SALAMI_path + 'first.csv', 10)
+        if sys.argv[2] == '-p':
+            GEN_VAL_SET = True
+        gen_cross_validation_files(SALAMI_path + 'first.csv', K)
     elif sys.argv[1] == '-v':
         if argc > 2:
             if sys.argv[2] == '-g':
                 BY_GAIN = True
-            elif sys.argv[2] == 'f':
+            elif sys.argv[2] == '-f':
                 BY_FREQ = True
-            elif sys.argv[2] == 'b':
+            elif sys.argv[2] == '-b':
                 BY_GAIN = True                
                 BY_FREQ = True
-        err = cross_validate(10)
+            elif sys.argv[2] == '-p':
+                POST_PRUNE = True
+        err = cross_validate(val_path, 1) # K
     elif sys.argv[1] == '-r':
         for i in range(1,11):
             test_tree(run_path + 'train_' + repr(i) + '.csv',
                       run_path + 'test_'  + repr(i) + '.csv')
-            print '*' * 10
-
+            print '**********'
